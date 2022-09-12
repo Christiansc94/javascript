7d8191498bc4c62d9ab9fbeaa1d2c7b536d7948e
Christian Sanchez <christian02064@gmail.com>
2022-09-12 18:32:59 -0500

    Adding Dom y eventos

--- js/simulador.js
+++ js/simulador.js
@@ -1,7 +1,3 @@
-// let totalCompra = 0
-// let selectedItem = parseInt(prompt('Ingresa el numero del producto que quieres comprar entre: 1.Anillo - 2.Collar - 3.Pulsera - 4.Tobillera - 5.Reloj'))
-// let seguirComprando = true
-// let choice
 let products = []
 let basket = []
 const selectItem = document.getElementById('list')
@@ -69,50 +65,3 @@ boton2.onclick = ()=>{
     })
     alert(`El valor a pagar es ${totalCompra}`)
 }
-
-
-// while (seguirComprando === true) {
-//     const requestedProduct = products.find(prod=>prod.id===selectedItem)
-//     if(requestedProduct){
-//         totalCompra = totalCompra + requestedProduct.precio
-//     }
-//     else {
-//         selectedItem = parseInt(prompt('Ingresaste un valor erroneo. 1.Anillo - 2.Collar - 3.Pulsera - 4.Tobillera - 5.Reloj'))
-//         continue
-//     }
-
-
-//     choice = parseInt(prompt('¿Quieres agregar algo mas? 1.Si - 2.No'))
-//     if (choice === 1) {
-//         selectedItem = parseInt(prompt('Ingresa el nombre del producto que quieres comprar entre: 1.Anillo - 2.Collar - 3.Pulsera - 4.Tobillera - 5.Reloj'))
-//     } else if (choice === 2) {
-//         seguirComprando = false
-//     }
-// }
-
-// alert('el valor total sin descuento ni impuesto es: ' + totalCompra)
-
-// function calcularPrecioConDescuento(valor) {
-//     let descuento = 0
-//     if (valor <= 100000) {
-//         descuento = 10
-//     } else if (valor > 100000 && valor <= 200000) {
-//         descuento = 15
-//     } else if (valor > 200000) {
-//         descuento = 20
-//     }
-//     let valorDescuento = valor * (descuento / 100)
-//     valor = valor - valorDescuento
-//     return valor
-// }
-// let valorConDescuento = calcularPrecioConDescuento(totalCompra)
-// alert('El total con descuento sin impuestos es: ' + valorConDescuento)
-
-// function calcularPrecioConImpuesto(valor) {
-//     const impuestos = valor * (9 / 100)
-//     return valor + impuestos
-// }
-
-// let valorFinalConImpuesto = calcularPrecioConImpuesto(valorConDescuento)
-// alert('El precio total incluyendo impuesto y descuento es de: ' + valorFinalConImpuesto)
-// alert('Gracias por tu compra')
